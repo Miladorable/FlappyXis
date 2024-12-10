91f58f4e9ca550731e7c24098b04be455d3202eb
Miladorable <Mila.Sales.Pro@gmail.com>
2024-12-10 16:32:35 +0100

    commencement

---
+++ Assets/Scripts/CharacterControler.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControler : MonoBehaviour
+{
+    [SerializeField]
+    private float jumpForce = 1f;
+    [SerializeField]
+    private Animator animator;
+
+    private string TriggerJump;
+    {
+
+    }
+}

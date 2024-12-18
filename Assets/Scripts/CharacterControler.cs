ad4b01d3caeb8b044af3b60109a242b51c70618b
Miladorable <Mila.Sales.Pro@gmail.com>
2024-12-18 16:39:40 +0100

    Assets et Jump

--- Assets/Scripts/CharacterControler.cs
+++ Assets/Scripts/CharacterControler.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterControler : MonoBehaviour
 {
+    [SerializeField]
+    private Rigidbody2D rigidbodyControl;
     [SerializeField]
     private float jumpForce = 1f;
     [SerializeField]
     private Animator animator;
 
-    private string TriggerJump;
-    {
 
+    public void Jump(InputAction.CallbackContext context)
+    {
+        rigidbodyControl.velocity = Vector3.zero;
+        rigidbodyControl.AddForce(transform.up * jumpForce);
+        
     }
+
+   // private string TriggerJump;
+    //{
+        
+    //}
 }
